bfac81540d85a0856929f23ffcc93593fa95dc6b
KintanSeprilSyayuni <114288995+KintanSeprilSyayuni@users.noreply.github.com>
2022-11-18 12:24:49 +0700

    Add files via upload

--- WarnetExample_Kintan.java
+++ WarnetExample_Kintan.java
@@ -10,5 +10,27 @@ package UTS_181122_A;
  * @author User
  */
 public class WarnetExample_Kintan {
+    WarnetRecord_Kintan warnet1 = new WarnetRecord_Kintan();
     
-}
+            student1.setNamaPelanggan("Padang");
+        student1.setJenisPelanggan(20);
+        student1.setTglMasuk(30);
+        student1.setJamMasuk();
+        student1.setJamKeluar();
+        student1.setTarif();
+        student1.setTotal();
+        student1.setLama();
+        student1.setdiskon(90);
+        //
+        
+        System.out.println("Kode                   :"+student1.getKode());
+        System.out.println("Nama Pelanggan         :"+student1.getNamaPelanggan());
+        System.out.println("jenis pelanggan        :"+student1.getJenisPelanggan());
+        System.out.println("Tangga Masuk           :"+student1.getTglMasuk());
+        System.out.println("Jam keluar             :"+student1.getJamMasuk());
+        System.out.println("Tarif                  :"+student1.getJamKeluar());
+        System.out.println("Total                  :"+student1.getTarif());
+        System.out.println("Lama                   :"+student1.getTotal());
+        System.out.println("Diskon                 :"+student1.getdiskon());
+        
+    }
